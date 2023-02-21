435c831c37b814976f395bd458bf61825fd94706
JMP160880 <javiermoranpando@gmail.com>
2023-02-21 19:58:44 +0000

    personaje

--- src/models.py
+++ src/models.py
@@ -9,11 +9,25 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Personaje(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+
+    
+    def __repr__(self):
+        return '<Personaje %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
         }
